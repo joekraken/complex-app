30977cc9ee61a197c836d8cc251d881830fee112
joe kirkbride <joseph.kirkbride@gmail.com>
2023-04-10 14:49:32 -0400

    initial app setup
    created app.js,  home view ejs, and main.css, organized folders
    
    initialzed npm package.json, installed modules: nodemon, express, ejs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# complex-app
+# Complex App
+
 Building a JS app for personal use, with todo feature, messaging system, and user login security.  Leveraging JavaScript, Express, Node, and Mongodb.
+
---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('app/public'))
+// setting the views folder locations
+app.set('views', 'app/views')
+// set view template engine
+app.set('view engine', 'ejs')
+
+// homepage GET request
+app.get('/', (req, res) => {
+  res.render('home-guest')
+})
+
+// listen to incoming requests
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
